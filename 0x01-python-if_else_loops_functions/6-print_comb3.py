791650a042ac3682095ed02fa3d308c01c84e705
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-09 17:38:46 +0000

    task 6 done - Inventing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
+for i in range(8):
+    for j in range(i + 1, 9):
         print('{}{}, '.format(i, j), end='')
-print('{}{}'.format(9, 0))
+print('{}{}'.format(8, 9))
