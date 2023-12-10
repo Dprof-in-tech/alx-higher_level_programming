75ef218ed135799cb0608f36d9f3bc4b7c713d52
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-10 00:56:16 +0000

    task 2 fixed...

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
+        print('{} arguments.'.format(len(argv) - 1), end='')
         num_arguments = len(argv) - 1
         if num_arguments > 1:
             print("{} argument{}:".format(num_arguments, 's'))
