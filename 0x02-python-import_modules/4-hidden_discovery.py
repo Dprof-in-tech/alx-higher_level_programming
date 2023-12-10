122e651d84987f754c1a90e57390de1941314319
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-10 06:35:58 +0000

    fixed task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,10 +7,7 @@ if __name__ == "__main__":
     with open('hidden_4.pyc', 'rb') as file:
         code = file.read()
 
-    code_obj = types.CodeType(
-            0, 0, 0, 0, 0, 0, b'', (), (), (), '', '', 0, b'')
-    code_obj = types.CodeType(*code_obj._fields,code)
-
+    code_obj = marshal.loads(code)
     names = set()
 
     for instruction in dis.get_instructions(code_obj):
