3ea855f31f944f376ae5c05581e2d44c0e829dbf
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-10 23:42:53 +0000

    task 1 done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     integers = my_list
     int_idx = integers[idx]
-    if int_idx < 0:
+    if idx < 0:
         return None
-    elif int_idx > len(integers):
+    elif idx > len(integers):
         return None
     else:
         return int_idx
