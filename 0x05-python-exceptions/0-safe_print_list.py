902b21e646884557971ff1879e305721eeb22193
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-19 04:33:37 +0000

    task 0 and 1 done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-    counter = 0;
+    counter = 0
+
     for i in my_list:
         if counter == x:
             break
 
         try:
             print("{} ".format(i), end='')
-            counter+= 1
+            counter += 1
         except (RuntimeError, TypeError, NameError):
             pass
 
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     if isinstance(value, int):
         try:
             print("{:d}".format(value))
             return True
-        except (NameError, TypeError, RuntimeError):
+        except (ValueError, TypeError):
             return False
-            pass
