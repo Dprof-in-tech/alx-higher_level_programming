867e8cbc96d369103b013fdf15417ba93628af50
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-19 06:01:46 +0000

    advanced task stderr message doen

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
         try:
-            print("{:d}".format(value))
+            int_value = int(value)
+            print("{:d}".format(int_value))
             return True
         except (ValueError, TypeError) as e:
             sys.stderr.write("Exception: {}".format(str(e)))
