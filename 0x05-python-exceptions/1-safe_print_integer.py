77fcc38cf030a6f19b0e43cf521cc6e591ae7fb4
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-19 04:27:18 +0000

    task 0 and 1 done

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    if isinstance(value, int):
+        try:
+            print("{:d}".format(value))
+            return True
+        except (NameError, TypeError, RuntimeError):
+            return False
+            pass
