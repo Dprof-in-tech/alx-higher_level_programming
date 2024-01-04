6d0020e9e3310ce6e6e69ce182928e0d2dbdb643
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-01-04 23:31:57 +0000

    class documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,12 @@
 
 
 class Square:
-    """This is a class to define a square
-
+    """
+    This is a class to define a square
     """
 
     def __init__(self):
+        """
+        Initialize a square with no sides or properties
+        """
         pass
