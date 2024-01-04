01d7af2ceda1af47edcb7f21fcea8556444e47e0
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-01-04 23:57:06 +0000

    suare size defined

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,4 @@ class Square:
     """Define suare"""
     def __init__(self, _Square__size):
         """initialize the size definintion"""
-        self._Square__size = 3
+        self._Square__size = _Square__size
