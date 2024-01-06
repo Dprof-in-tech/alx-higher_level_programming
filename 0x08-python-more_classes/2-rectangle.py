9900c88e1e4aec40505885f0376e23d279537986
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-01-06 22:43:10 +0000

    fixed rectangle 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self, value):
+    def width(self):
         return self.__width
 
     @width.setter
@@ -22,7 +22,7 @@ class Rectangle:
         self.__width = value
 
     @property
-    def height(self, value):
+    def height(self):
         return self.__height
 
     @height.setter
@@ -34,7 +34,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """return area of square"""
+        """return area of rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
