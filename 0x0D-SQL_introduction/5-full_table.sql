bd92ad1f5f3b068e9002c9c62a8f253695904b31
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-02-15 18:15:53 +0000

    5th commit

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,8 @@
+--SQL query to describe a database table without using the describe or explain keyword
+
+
+USE information_schema;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+--SQL Query to list all values from a table column
+
+USE hbtn_0c_0;
+
+SELECT * FROM first_table;
