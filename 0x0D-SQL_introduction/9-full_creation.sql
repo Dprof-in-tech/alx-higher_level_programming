1d627225e3584a56f5131cb7294dbb16a8c3b188
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-02-15 21:02:34 +0000

    9th commit

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- SQL Query to create a full table with multiple columns
+
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
+	score INT NOT NULL
+);
