aee08056a8432c1de822c230d09da0a6998b80e2
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-02-15 21:51:34 +0000

    14th commit

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- SQL Query to compute the average of all scores in the records
 
-WITH average AS (SELECT AVG(score) FROM second_table) UPDATE second_table SET average = average;
+WITH average AS (SELECT AVG(score) AS avg_score FROM second_table) UPDATE second_table JOIN average SET second_table.average = average.avg_score;
 
