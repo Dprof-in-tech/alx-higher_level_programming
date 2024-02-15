e33a61873bea735e0933fb759889c3494df56f31
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-02-15 21:08:20 +0000

    9th commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,3 +5,5 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256) NOT NULL,
 	score INT NOT NULL
 );
+
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
